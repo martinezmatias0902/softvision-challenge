e3d0b6ae7c5f8b145a9d77fcdd7b19a819729c80
Matias Martinez <martinez.matias0902@gmail.com>
2022-02-20 12:07:04 -0300

    update and add candidates

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -18,6 +18,39 @@ function App() {
   const [candidates, setCandidates] = useState<Candidate[]>([]);
   const [status, setStatus] = useState<"init" | "resolved">("init");
 
+  function updateCandidate(id: Candidate["id"], partial: Partial<Candidate>) {
+    setCandidates((candidates) =>
+      candidates.map((candidate) =>
+        candidate.id == id
+          ? {
+              ...candidate,
+              ...partial,
+            }
+          : candidate,
+      ),
+    );
+  }
+
+  function editCandidate(id: Candidate["id"], partial: Partial<Candidate>) {
+    return 0;
+  }
+
+  function addCandidate() {
+    const candidate = window.prompt("Nombre: ");
+    const comments = window.prompt("Comentarios: " || "");
+
+    if (!candidate) return;
+
+    setCandidates((candidates) =>
+      candidates.concat({
+        id: String(+new Date()),
+        name: candidate,
+        comments,
+        step: "Entrevista inicial",
+      }),
+    );
+  }
+
   useEffect(() => {
     api.candidates.list().then((candidates) => {
       setCandidates(candidates);
@@ -31,23 +64,59 @@ function App() {
 
   return (
     <main className={styles.columns}>
-      {STEPS.map((step) => (
+      {STEPS.map((step, index) => (
         <section key={step} className={styles.column}>
           <h1>{step}</h1>
           {candidates
             .filter((candidate) => candidate.step == step)
             .map((candidate) => (
               <article key={candidate.id} className={styles.card}>
-                <div>
-                  <p className={styles.candidate}>{candidate.name}</p>
-                  {candidate.comments && <p className={styles.comments}>{candidate.comments}</p>}
-                </div>
-                <div>
-                  <button>{"<"}</button>
-                  <button>{">"}</button>
+                <div className={styles.content}>
+                  <div>
+                    <p className={styles.candidate}>{candidate.name}</p>
+                    {candidate.comments && (
+                      <p
+                        className={styles.comments}
+                        onClick={() =>
+                          updateCandidate(candidate.id, {
+                            comments:
+                              window.prompt("Comentarios del candidato: ", candidate.comments) ||
+                              "",
+                          })
+                        }
+                      >
+                        {candidate.comments}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    {index > 0 && (
+                      <button
+                        onClick={() =>
+                          updateCandidate(candidate.id, {
+                            step: STEPS[index - 1],
+                          })
+                        }
+                      >
+                        {"<"}
+                      </button>
+                    )}
+                    {index < STEPS.length - 1 && (
+                      <button
+                        onClick={() =>
+                          updateCandidate(candidate.id, {
+                            step: STEPS[index + 1],
+                          })
+                        }
+                      >
+                        {">"}
+                      </button>
+                    )}
+                  </div>
                 </div>
               </article>
             ))}
+          {index == 0 && <button onClick={addCandidate}>Agregar Candidato</button>}
         </section>
       ))}
     </main>
